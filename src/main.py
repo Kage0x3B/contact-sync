189af48800eb57c7ecbe9d26b4f23eabeb227518
Moritz Hein <moritz.hein@live.de>
2021-03-29 12:57:44 +0200

    Testing docspell auth etc in main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,27 @@
+import os
+from dotenv import load_dotenv
+from carddav_importer import CardDavImporter
+from docspell_api import DocspellApi
+import vcard_reader
+from src import docspell_api
+
+
+def main():
+    load_dotenv()
+
+    carddav_importer = CardDavImporter(os.getenv("WEBDAV_HOST"), os.getenv("WEBDAV_LOGIN"),
+                                       os.getenv("WEBDAV_PASSWORD"), os.getenv("CARDDAV_PATH"))
+
+    carddav_importer.connect()
+    # TODO For testing only, downloading all takes pretty long
+    # carddav_importer.download_contacts()
+
+    docspell = DocspellApi(os.getenv("DOCSPELL_HOST"))
+    docspell.authenticate(os.getenv("DOCSPELL_USERNAME"), os.getenv("DOCSPELL_PASSWORD"))
+
+    print("categories:", vcard_reader.list_category_names())
+    # vcard_reader.load_from_temp()
+
+
 if __name__ == "__main__":
-    pass
+    main()
